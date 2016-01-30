20dda58d71be531ecb41bcccb9d3b12f22256279
RuslanSharaf <sharafievruslanfail@gmail.com>
2016-01-30 23:22:25 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
-<?
-phpinfo();
+<?php
+class Animal {
+    function makeSound()
+    {
+        print "Ошибка: Этот метот должен быть перекрыт в дочернем классе";
+    
+    }
+}
+class Cat extends Animal {
+    function makeSound() 
+    {
+        print "мяу";
+                
+    }
+}
+class Dog extends Animal {
+    function makeSound() 
+    {
+        print "гав";
+        
+    }
+function printTheRightSound($obj) {
+{
+    if ($obj instanceof Animal) {
+        $obj->makeSound();
+    } else {
+        print "Ошибка: Неопознанный объект";
+    }
+    print "\n";
+}
+printTheRightSound(new Cat());
+printTheRightSound(new Dog());
+}
+    
+}
 ?>
